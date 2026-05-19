48c58b8dcb664081765a65f924178048b34ad397
EmillyMourac <emillymoura.0501@gmail.com>
2026-05-19 14:15:32 -0300

    feat:entregando atv, metodo c# atualizado

--- Atividade02.cs
+++ Atividade02.cs
@@ -4,4 +4,127 @@ estudar tabuada. O sistema deve permitir que o usuário escolha um número e exi
 a tabuada de 1 até 10.
 */
 
+// static int LerNumeroTabela()
+// {
+//     Console.WriteLine("Digite o numero da tabuada:");
+//     int numero = int.Parse(Console.ReadLine()!);
+//     return numero;
+// }
 
+// static void ExibirTabuada(int numero)
+// {
+//     Console.WriteLine();
+
+//     int contador = 1;
+
+//     while (contador <= 10)
+//     {
+//         Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+//         contador++;
+//     }
+
+//     Console.WriteLine();
+// }
+
+// static string LerContinuacao()
+// {
+//     Console.WriteLine("deseja consoltar outra tabuada? (s/n)");
+//     string resposta = Console.ReadLine()!;
+
+//     return resposta;
+// }
+
+// static bool DesejaContinuar(string resposta)
+// {
+//     if (resposta == "s" || resposta == "S")
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+
+// static void ExecultarTabuada()
+// {
+//     Console.Clear();
+
+//     int numero = LerNumeroTabela();
+//     ExibirTabuada(numero);
+
+//     string resposta = LerContinuacao();
+
+//     if (DesejaContinuar(resposta))
+//     {
+//         ExecultarTabuada();
+//     }
+//     else
+//     {
+//         Console.WriteLine("programa encerrado.");
+//     }
+// }
+// ExecultarTabuada();
+
+
+// static int LerNumeroTabela()
+// {
+//     Console.WriteLine("Digite o numero da tabuada:");
+//     int numero = int.Parse(Console.ReadLine()!);
+//     return numero;
+// }
+
+// static void ExibirTabuada(int numero)
+// {
+//     Console.WriteLine();
+
+//     int contador = 1;
+
+//     while (contador <= 10)
+//     {
+//         Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+//         contador++;
+//     }
+
+//     Console.WriteLine();
+// }
+
+// static string LerContinuacao()
+// {
+//     Console.WriteLine("deseja consoltar outra tabuada? (s/n)");
+//     string resposta = Console.ReadLine()!;
+
+//     return resposta;
+// }
+
+// static bool DesejaContinuar(string resposta)
+// {
+//     if (resposta == "s" || resposta == "S")
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+
+// static void ExecultarTabuada()
+// {
+//     Console.Clear();
+
+//     int numero = LerNumeroTabela();
+//     ExibirTabuada(numero);
+
+//     string resposta = LerContinuacao();
+
+//     if (DesejaContinuar(resposta))
+//     {
+//         ExecultarTabuada();
+//     }
+//     else
+//     {
+//         Console.WriteLine("programa encerrado.");
+//     }
+// }
+// ExecultarTabuada();
